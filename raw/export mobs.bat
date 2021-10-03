63a0269547815dae01457604949107209fc94987
John Hampson <jonnohampson@gmail.com>
2021-10-03 17:24:38 +0200

    added mob types and cleaned up class names

--- raw/export mobs.bat
+++ raw/export mobs.bat
@@ -1,1 +1,1 @@
-aseprite -b --ignore-empty "D:\dev\java\ludumdare\49\projects\LD49\raw\spritesheets\submarine.aseprite" --save-as "D:\dev\java\ludumdare\49\projects\LD49\raw\spritesheets\mobs\{layer}\{tag} 00.png"
+aseprite -b --ignore-empty "D:\dev\java\ludumdare\49\projects\LD49\raw\textures\mobs.aseprite" --save-as "D:\dev\java\ludumdare\49\projects\LD49\raw\spritesheets\mobs\{layer}\{tag} 00.png"
